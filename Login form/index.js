e7393f943ebf9b961221fcf224a20f2aa81cc13e
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-03-18 14:34:11 +0530

    Create index.js

---
+++ Login form/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $( ".input" ).focusin(function() {
+        $( this ).find( "span" ).animate({"opacity":"0"}, 200);
+      });
+      
+      $( ".input" ).focusout(function() {
+        $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+      });
+      
+      $(".login").submit(function(){
+        $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
+        $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
+        $("input").css({"border-color":"#2ecc71"});
+        return false;
+      });  
+});
